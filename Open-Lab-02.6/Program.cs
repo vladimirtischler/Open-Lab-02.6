ce552ff5cd7dad077d250187cfbba2808e379642
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-20 19:14:33 +0200

    open-lab-02.6

--- Open-Lab-02.6/Program.cs
+++ Open-Lab-02.6/Program.cs
@@ -4,9 +4,15 @@ namespace Open_Lab_02._6
 {
     class Program
     {
+        public static bool Divisible(int a)
+        {
+            return (a % 100 == 0);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Divisible(b));
         }
     }
 }
