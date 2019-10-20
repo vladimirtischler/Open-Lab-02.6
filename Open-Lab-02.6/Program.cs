4e1e0beaab87fea524b1f5d2554023b35e6700a8
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-20 19:13:09 +0200

    open-lab-02.6

---
+++ Open-Lab-02.6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_02._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
